e54b75b794cdcdf0b24ba02b7a0ef2323a1087b3
parnob <parnob.keystone@gmail.com>
2025-06-20 17:22:53 +0700

    Update app.py

--- app.py
+++ app.py
@@ -14,3 +14,19 @@ def hello():
 def echo():
     data = request.get_json()
     return jsonify({"you_sent": data})
+
+
+
+@app.route('/rnn', methods=['POST'])
+def rnn_predict():
+    data = request.json
+    text_input = data.get('text', '')
+    # Here you would call your RNN model to predict based on text_input
+    # For demo, just echo the input reversed as “prediction”
+    prediction = text_input[::-1]  
+    return jsonify({'input': text_input, 'prediction': prediction})
+
+if __name__ == '__main__':
+    import os
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host='0.0.0.0', port=port)
