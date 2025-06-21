941dc59c2c473b52ea2b487f496cc4f6974a968a
parnob <parnob.keystone@gmail.com>
2025-06-21 16:56:24 +0700

    Update app.py

--- app.py
+++ app.py
@@ -15,26 +15,3 @@ async def rnn_predict(data: RNNInput):
     prediction = text_input[::-1]
     return {"input": text_input, "prediction": prediction}
 
-
-#from flask import Flask, jsonify, request
-
-#app = Flask(__name__)
-
-#@app.route('/')
-#def home():
-#    return "✅ Flask API is working!"
-
-
-#@app.route('/rnn', methods=['POST'])
-#def rnn_predict():
-#    data = request.json
-#    text_input = data.get('text', '')
-    # Here you would call your RNN model to predict based on text_input
-    # For demo, just echo the input reversed as “prediction”
-#    prediction = text_input[::-1]  
-#    return jsonify({'input': text_input, 'prediction': prediction})
-
-#if __name__ == '__main__':
-#    import os
-#    port = int(os.environ.get("PORT", 10000))
-#    app.run(host='0.0.0.0', port=port)
